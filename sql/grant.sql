e6b5bfd0e9027839f71dfad00e17dd4063c9f1e6
root <root@s222637.hostiman.com>
2022-04-28 17:50:33 +0300

    +grant.sql

---
+++ sql/grant.sql
@@ -0,0 +1,2 @@
+CREATE USER `apvuser`@`%` IDENTIFIED BY '12345678qwerty'; 
+GRANT ALL PRIVILEGES ON `apv`.* TO `apvuser`@`%`;
--- sql/pattern_tables.sql
+++ sql/pattern_tables.sql
@@ -7,10 +7,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Структура таблицы `pattern_tokens`
---
-
 CREATE TABLE `pattern_tokens` (
   `lts` datetime NOT NULL DEFAULT current_timestamp(),
   `ats` datetime NOT NULL DEFAULT current_timestamp(),
@@ -18,9 +14,10 @@ CREATE TABLE `pattern_tokens` (
   `uid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Структура таблицы `pattern_users`
---
+
+ALTER TABLE `pattern_tokens`
+  ADD PRIMARY KEY (`token`);
+COMMIT;
 
 CREATE TABLE `pattern_users` (
   `uid` int(11) NOT NULL,
@@ -32,43 +29,17 @@ CREATE TABLE `pattern_users` (
   `extended` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`extended`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Дамп данных таблицы `pattern_users`
---
-
-INSERT INTO `pattern_users` (`uid`, `email`, `roles`, `blocked`, `confirmed`, `pass_hash`, `extended`) VALUES
-(1, 'admin', '[\"admin\",\"default\"]', 0, 1, 'e10adc3949ba59abbe56e057f20f883e', '{\"name\":\"Админ\",\"position\":\"Админ\",\"phone\":\"admin\",\"secondName\":\"Админов\",\"ava_url\":\"no_ava.jpg\"}'),
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `pattern_users`
---
-
 ALTER TABLE `pattern_users`
   ADD PRIMARY KEY (`uid`),
   ADD UNIQUE KEY `email` (`email`) USING HASH;
 
---
--- Индексы таблицы `pattern_tokens`
---
 
-ALTER TABLE `pattern_tokens`
-  ADD PRIMARY KEY (`token`);
-COMMIT;
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `pattern_users`
---
 ALTER TABLE `pattern_users`
   MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142;
 COMMIT;
+
+INSERT INTO `pattern_users` (`uid`, `email`, `roles`, `blocked`, `confirmed`, `pass_hash`, `extended`) VALUES
+(1, 'admin', '[\"admin\",\"default\"]', 0, 1, 'e10adc3949ba59abbe56e057f20f883e', '{\"name\":\"Админ\",\"position\":\"Админ\",\"phone\":\"admin\",\"secondName\":\"Админов\"}');
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
